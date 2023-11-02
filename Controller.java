8865c7faeaed60d53bb6f7f0d0511f067c749ef7
cl8502 <cl8502@g.rit.edu>
2023-11-02 08:48:52 -0400

    Finished helpmsg function

--- Controller.java
+++ Controller.java
@@ -4,10 +4,16 @@ class Controller {
     static Scanner sc;
 
     private static void print_help() {
-        System.out.println("1. Add Music");
-        System.out.println("2. Delete Music");
-        System.out.println("3. Add Playlist");
-        System.out.println("4. Delete Playist");
+        System.out.println("Actions avaliable:");
+        System.out.println("\t1. Add Song to Collection");
+        System.out.println("\t2. Delete Song to Collection");
+        System.out.println("\t3. Add Album to Collection");
+        System.out.println("\t4. Delete Album to Collection");
+        System.out.println("\t5. Follow User");
+        System.out.println("\t6. Unfollow User");
+        System.out.println("\t7. View Collection Of Songs");
+        System.out.println("\t8. Change name of Collection");
+        System.out.println("\t9. Delete whole Collection");
     }
 
     private static boolean create_account() {
